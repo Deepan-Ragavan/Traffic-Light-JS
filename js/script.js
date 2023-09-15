2dc35f43855c1224c0c4fdf672c2bf90de2235f0
Deepan-Ragavan <deebanyathiraja2002@gmail.com>
2023-09-15 22:07:38 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let stopDiv = document.querySelector('.stop')
+let readyDiv = document.querySelector('.ready')
+let goDiv = document.querySelector('.go')
+
+
+let stop = () =>{
+    stopDiv.style.background ='red'
+    readyDiv.style.background ='rgb(27, 27, 39)'
+    goDiv.style.background ='rgb(27, 27, 39)'
+}
+
+let ready = () =>{
+    stopDiv.style.background ='rgb(27, 27, 39)'
+    readyDiv.style.background ='yellow'
+    goDiv.style.background ='rgb(27, 27, 39)'
+}
+
+let go = () =>{
+    stopDiv.style.background ='rgb(27, 27, 39)'
+    readyDiv.style.background ='rgb(27, 27, 39)'
+    goDiv.style.background ='green'
+}
