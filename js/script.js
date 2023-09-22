441e89e574e95a8f884adecce1e81d32cae6f008
Deepan-Ragavan <deebanyathiraja2002@gmail.com>
2023-09-22 21:58:14 +0530

    Modified to be responsive

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
 let stopDiv = document.querySelector('.stop')
 let readyDiv = document.querySelector('.ready')
 let goDiv = document.querySelector('.go')
+let container = document.querySelector('.container')
 
-
-let stop = () =>{
-    stopDiv.style.background ='red'
-    readyDiv.style.background ='rgb(27, 27, 39)'
-    goDiv.style.background ='rgb(27, 27, 39)'
+container.onclick = () => {
+    stopDiv.style.background = 'rgb(27, 27, 39)'
+    readyDiv.style.background = 'rgb(27, 27, 39)'
+    goDiv.style.background = 'rgb(27, 27, 39)'
 }
 
-let ready = () =>{
-    stopDiv.style.background ='rgb(27, 27, 39)'
-    readyDiv.style.background ='yellow'
-    goDiv.style.background ='rgb(27, 27, 39)'
+let stop = () => {
+    stopDiv.style.background = 'red'
+    readyDiv.style.background = 'rgb(27, 27, 39)'
+    goDiv.style.background = 'rgb(27, 27, 39)'
 }
 
-let go = () =>{
-    stopDiv.style.background ='rgb(27, 27, 39)'
-    readyDiv.style.background ='rgb(27, 27, 39)'
-    goDiv.style.background ='green'
+let ready = () => {
+    stopDiv.style.background = 'rgb(27, 27, 39)'
+    readyDiv.style.background = 'yellow'
+    goDiv.style.background = 'rgb(27, 27, 39)'
+}
+
+let go = () => {
+    stopDiv.style.background = 'rgb(27, 27, 39)'
+    readyDiv.style.background = 'rgb(27, 27, 39)'
+    goDiv.style.background = 'green'
 }
